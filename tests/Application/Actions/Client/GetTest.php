72b1cc70ec7d1f8f65918adb9f561730cb4f8be6
Alexandre Melo <alexandrecarvalhodemelo@outlook.com>
2019-08-26 15:27:41 -0300

    adding test cases

--- tests/Application/Actions/Client/GetTest.php
+++ tests/Application/Actions/Client/GetTest.php
@@ -209,13 +209,12 @@ class GetTest extends TestCase
             ]
         );
 
-        /*
         $response = $this->createRequest('GET', $url);
         $responseBodyJson = json_decode($response->getBody()->getContents());
-        $this->writeMessage('Testing invalid client '. $url);
+        $this->writeMessage('Testing year with spacex '. $url);
         $this->assertEquals(404, $response->getStatusCode());
         $this->assertContains('RESOURCE_NOT_FOUND', $responseBodyJson->error->type);
-        */
+
     }
 
     /**
@@ -243,12 +242,12 @@ class GetTest extends TestCase
             ]
         );
 
-        /*
+
         $response = $this->createRequest('GET', $url);
         $responseBodyJson = json_decode($response->getBody()->getContents());
-        $this->writeMessage('Testing invalid client '. $url);
+        $this->writeMessage('Testing year with xkcd '. $url);
         $this->assertEquals(404, $response->getStatusCode());
         $this->assertContains('RESOURCE_NOT_FOUND', $responseBodyJson->error->type);
-        */
+
     }
 }
