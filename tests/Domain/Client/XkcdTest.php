9576a6a9d21836a50b4ec95830ff82e561f2caa9
Alexandre Melo <alexandrecarvalhodemelo@outlook.com>
2019-08-25 23:58:30 -0300

    Unit testing

--- tests/Domain/Client/XkcdTest.php
+++ tests/Domain/Client/XkcdTest.php
@@ -1,59 +1,88 @@
 <?php
-//declare(strict_types=1);
-//
-//namespace Tests\Domain\Client;
-//
-//use App\Domain\Client\Client;
-//use Tests\TestCase;
-//
-//class SpacexTest extends TestCase
-//{
-//    public function clientProvider()
-//    {
-//        return [
-//            [1, 'bill.gates', 'Bill', 'Gates'],
-//            [2, 'steve.jobs', 'Steve', 'Jobs'],
-//            [3, 'mark.zuckerberg', 'Mark', 'Zuckerberg'],
-//            [4, 'evan.spiegel', 'Evan', 'Spiegel'],
-//            [5, 'jack.dorsey', 'Jack', 'Dorsey'],
-//        ];
-//    }
-//
-//    /**
-//     * @dataProvider userProvider
-//     * @param $id
-//     * @param $username
-//     * @param $firstName
-//     * @param $lastName
-//     */
-//    public function testGetters($id, $username, $firstName, $lastName)
-//    {
-//        $user = new Client($id, $username, $firstName, $lastName);
-//
-//        $this->assertEquals($id, $user->getId());
-//        $this->assertEquals($username, $user->getUsername());
-//        $this->assertEquals($firstName, $user->getFirstName());
-//        $this->assertEquals($lastName, $user->getLastName());
-//    }
-//
-//    /**
-//     * @dataProvider userProvider
-//     * @param $id
-//     * @param $username
-//     * @param $firstName
-//     * @param $lastName
-//     */
-//    public function testJsonSerialize($id, $username, $firstName, $lastName)
-//    {
-//        $user = new Client($id, $username, $firstName, $lastName);
-//
-//        $expectedPayload = json_encode([
-//            'id' => $id,
-//            'username' => $username,
-//            'firstName' => $firstName,
-//            'lastName' => $lastName,
-//        ]);
-//
-//        $this->assertEquals($expectedPayload, json_encode($user));
-//    }
-//}
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Client;
+
+use Ams\Domain\Client\Adapter\Xkcd;
+use Buzz\Client\Curl;
+use Buzz\Client\MultiCurl;
+use Cache\Adapter\Predis\PredisCachePool;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Tests\TestCase;
+
+class XkcdTest extends TestCase
+{
+
+    /**
+     * @var array
+     */
+    protected $request = [
+        'year' => 2018,
+        'limit' => 20,
+    ];
+
+    /**
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function testCanCallCacheClass(){
+        $spaceClass = new Xkcd();
+        $this->writeMessage('Testing can access cache class');
+        $this->assertInstanceOf(PredisCachePool::class, $spaceClass->getCache());
+    }
+
+    /**
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function testCanCallHttpClientClass(){
+        $spaceClass = new Xkcd();
+        $this->writeMessage('Testing can access http class');
+        $requestFactory = new Psr17Factory();
+        $this->assertInstanceOf(Curl::class, $spaceClass->getHttpClient($requestFactory));
+    }
+
+    /**
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function testCanCallMultiHttpClientClass(){
+        $spaceClass = new Xkcd();
+        $this->writeMessage('Testing can access multi http class');
+        $requestFactory = new Psr17Factory();
+        $this->assertInstanceOf(MultiCurl::class, $spaceClass->getHttpMultiClient($requestFactory));
+    }
+
+    /**
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function testCanMakeRequest(){
+        $spaceClass = new Xkcd();
+        $this->writeMessage('Testing can make a request');
+        $requestFactory = new Psr17Factory();
+        $client =  $spaceClass->getHttpMultiClient($requestFactory);
+        $request = $requestFactory->createRequest(
+            'GET',
+            'https://amsapi.herokuapp.com/api/client/comics/year/2008/limit/1'
+        );
+
+        $response = $client->sendRequest($request);
+        $body = json_decode($response->getBody()->getContents());
+        $data = $body->meta->request->data[0];
+
+        $this->writeMessage('Status code should be 200');
+        $this->assertEquals(self::HTTP_SUCCESS_CODE, $response->getStatusCode());
+
+        $this->writeMessage('Response body must have right fields');
+        $this->assertObjectHasAttribute('number', $data);
+        $this->assertObjectHasAttribute('name', $data);
+        $this->assertObjectHasAttribute('date', $data);
+        $this->assertObjectHasAttribute('link', $data);
+        $this->assertObjectHasAttribute('details', $data);
+
+    }
+
+
+}
